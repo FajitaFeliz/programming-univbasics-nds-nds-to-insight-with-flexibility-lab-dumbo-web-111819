6e9494ba99ab1d7c1c13cf1b32448c086e0cbbc2
Ben Miriello <benmiriello@gmail.com>
2019-11-10 15:50:23 +0000

    Automatically backed up by Learn

--- spec/nds_extract_spec.rb
+++ spec/nds_extract_spec.rb
@@ -1,37 +1,37 @@
 require 'spec_helper'
 
-describe 'provided method' do
-  describe 'flatten_a_o_a' do
-    it 'flattens an a_a_a of Integers' do
-      expect(flatten_a_o_a([[1,2],[3,4,5],[6]])).to eq([1,2,3,4,5,6])
-    end
-  end
+# describe 'provided method' do
+#   describe 'flatten_a_o_a' do
+#     it 'flattens an a_a_a of Integers' do
+#       expect(flatten_a_o_a([[1,2],[3,4,5],[6]])).to eq([1,2,3,4,5,6])
+#     end
+#   end
 
-  describe 'movie_with_director_name' do
-    it "creates a new movie Hash with director's name provided" do
-      director_name = "Byron Poodle"
-      movies_coll = {
-        :worldwide_gross => 2,
-        :release_year => 2014,
-        :studio => "Karbit Poodles",
-        :title => "The Fire Hydrant of Doom"
-      }
-      new_movie = movie_with_director_name(director_name, movies_coll)
+#   describe 'movie_with_director_name' do
+#     it "creates a new movie Hash with director's name provided" do
+#       director_name = "Byron Poodle"
+#       movies_coll = {
+#         :worldwide_gross => 2,
+#         :release_year => 2014,
+#         :studio => "Karbit Poodles",
+#         :title => "The Fire Hydrant of Doom"
+#       }
+#       new_movie = movie_with_director_name(director_name, movies_coll)
 
-      expect(new_movie[:director_name]).to eq(director_name)
-    end
-  end
-end
+#       expect(new_movie[:director_name]).to eq(director_name)
+#     end
+#   end
+# end
 
-describe 'movies_with_director_key' do
-  it 'adds a :director_name key an AoH of movies' do
-    dir_name = "Byron Poodle"
-    test_set = [{:title => "TestA"}, {:title => "TestB"}]
-    updated_movies = movies_with_director_key(dir_name, test_set)
-    expect(updated_movies[0][:director_name]).to eq(dir_name), "Should add director name to each movie Hash"
-    expect(updated_movies[1][:director_name]).to eq(dir_name), "Should add director name to each movie Hash"
-  end
-end
+# describe 'movies_with_director_key' do
+#   it 'adds a :director_name key an AoH of movies' do
+#     dir_name = "Byron Poodle"
+#     test_set = [{:title => "TestA"}, {:title => "TestB"}]
+#     updated_movies = movies_with_director_key(dir_name, test_set)
+#     expect(updated_movies[0][:director_name]).to eq(dir_name), "Should add director name to each movie Hash"
+#     expect(updated_movies[1][:director_name]).to eq(dir_name), "Should add director name to each movie Hash"
+#   end
+# end
 
 describe 'movies_with_directors_set' do
   describe 'when given a Hash with keys :name and :movies,' do
